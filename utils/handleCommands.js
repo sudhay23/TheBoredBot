b17404d6cd30ada348cfbe522b0204090ddfd386
sudhay23 <sudhay2001@gmail.com>
2022-03-21 23:24:51 +0530

    Customized the /help command message

--- utils/handleCommands.js
+++ utils/handleCommands.js
@@ -8,7 +8,15 @@ export default async (interaction, commandName, options) => {
     });
 
     await interaction.editReply({
-      content: "HELP_TODO",
+      content: `
+      **TheBoredBot**
+      A bot that kills boredom giving you new websites to explore. Inspired by and courtesy of BoredButton
+      
+      [Invite me to another server](https://discord.com/api/oauth2/authorize?client_id=955456643745845288&permissions=534723950656&scope=applications.commands%20bot)
+      
+      ***What it can do?***
+       - Anybody of the server can use the \`/bored\` command and the bot gives you one cool website to check out which you may never have seen before
+      `,
     });
   } else if (commandName === "contribute") {
     const contributeEmbed = new MessageEmbed()
